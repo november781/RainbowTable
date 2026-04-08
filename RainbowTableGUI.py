bf4e00ae353fc8abccb0380a4a6f38bc21e51c06
framework <framework@nolan-clark.com>
2026-04-08 10:43:38 -0400

    Start work on a TKinter GUI for the project

---
+++ RainbowTableGUI.py
@@ -0,0 +1,15 @@
+from rainbowtable import *
+import tkinter as tk
+
+class RainbowTableGUI:
+    def __init__(self):
+        # instantiate main window and title
+        self.root = tk.Tk()
+        self.root.title("Rainbow Table")
+
+
+
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    start = RainbowTableGUI()
